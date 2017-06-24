8f43c46528e59f12b619df282d09d6bdb9fe6374
javirk <tuzine.es@gmail.com>
2017-06-24 18:36:53 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -8,31 +8,40 @@ rutaconf='./Settings/settings.ini'
 
 sistema, red, ejecucion, version = fun.todoConfiguracion(rutaconf)
 
+#La carpeta donde se van a guardar los resultados se crea dentro de la carpeta Montecarlo y depende de la fecha
+#Si da error es porque ya existe una carpeta con el mismo nombre en la ruta
 nombre = fun.sacaNombre()
 carpeta = r'Resultados/Montecarlo/'+nombre+red['tipored']
 os.makedirs(carpeta)
 
 Npueblos = red['nredes']
 
-#for b in [float(j) * 0.25 for j in range(1, 4, 1)]:
-for beta in [float(l) / 10 for l in range(1, 8, 2)]:
-    pueblos = [pc.Town(rutaconf=rutaconf, ired=i) for i in range(Npueblos)]
-    b = sistema['b']
-    for pueblo in pueblos:
-        pueblo.evoluciona(b, beta)
-        if ejecucion['tipometodo'] == 'tiempo':
-            pueblo.analiza()
-        pueblo.escribeTodo(carpeta, b, beta, sistema['r'])
-        #pueblo.representaPar()
+#Crea tantos pueblos como se necesite
+pueblos = [pc.Town(rutaconf=rutaconf, ired=i) for i in range(Npueblos)]
+b = sistema['b']
+beta = sistema['beta']
+for pueblo in pueblos:
+    # Simula con el método de Monte Carlo. Evoluciona es un método que también termaliza. Guarda los datos en propC
+    # en el caso de que ejecucion['tipometodo'] = 'tiempo'. Si no, en la matriz mediaParC
+    pueblo.evoluciona(b, beta)
+    if ejecucion['tipometodo'] == 'tiempo':
+        # Analiza los resultados de ese pueblo (media, varianza...). No es necesario pasar argumentos porque propC
+        # son variables locales de la clase
+        pueblo.analiza()
+    pueblo.escribeTodo(carpeta, b, beta, sistema['r'])
+    #pueblo.representaPar()
+
+# Lo siguiente es para escribir los resultados
 
 if ejecucion['tipometodo'] == 'tiempo':
+    #Si se quiere dibujar la gráfica
     if ejecucion['grafica']:
         fun.dibujaGrafica(pueblos)
-        print(fun.mediaTiempo(pueblos, graf=False))
-    else:
-        print(fun.mediaTiempo(pueblos, graf = ejecucion['grafica']))
+    print(fun.mediaTiempo(pueblos, graf=False))
+
 else:
     if Npueblos > 1:
+        # Se media entre todos los pueblos
         mediaTotalParC = [[0 for i in range(ejecucion['cantidad2'])] for j in range(ejecucion['cantidad1'])]
         parametro1 = list(pueblos[0].parametro1)
         for i in range(Npueblos):
